67841939f1f9072115d3705751c3051bf3dd1234
Zainab Saud <zai99nb@gmail.com>
2020-07-09 15:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* Your Code Here */
 
-const employee =["Gray", "Worm","Security",1]
-
 function createEmployeeRecord(employee){
 return { firstName :employee[0],
           familyName:employee [1],
@@ -12,51 +10,6 @@ return { firstName :employee[0],
 }
 }
 
-let twoRows = [
-        ["moe", "sizlak", "barkeep", 2],
-        ["bartholomew", "simpson", "scamp", 3]
-      ]
-
-function createEmployeeRecords(twoRows){
-  return  twoRows.map(row => createEmployeeRecord(row))
-  }
-
-function createTimeInEvent(employee ,Date){
-  let [date , hour]= Date.split(' ')
-  
-  employee.timeInEvents.push({
-    type:"TimeIn",
-    date:date,
-    hour:parseInt(hour,10)
-    
-  })
-  return employee
-}
-
-
-function createTimeOutEvent(employee ,Date){
-  let [date , hour]= Date.split(' ')
-  
-  employee.timeOutEvents.push({
-    type:"TimeOut",
-    date:date,
-    hour:parseInt(hour,10)
-    
-  })
-  return employee
-}
-
-
-function hoursWorkedOnDate(employee,reqDate){
-  let timeIn = employee.timeInEvents.find(event=>event.date===reqDate)
- let timeOut = employee.timeOutEvents.find(event=>event.date===reqDate)
- 
- return (timeOut.hour - timeIn.hour)/100
-}
-
-function wagesEarnedOnDate(employee,reqDate){
-  return hoursWorkedOnDate(employee,reqDate)*employee.payPerHour
-}
 
 
 
