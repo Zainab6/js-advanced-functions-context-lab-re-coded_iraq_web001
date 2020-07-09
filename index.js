6a4c926a16ad3466b1c324632daebda413c756b0
Zainab Saud <zai99nb@gmail.com>
2020-07-09 15:12:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 /* Your Code Here */
 
+const employee =["Gray", "Worm","Security",1]
+function createEmployeeRecord (employee){
+return { firstName :employee[0],
+          familyName:employee [1],
+          title :employee[2],
+          payPerHour :employee[3],
+          timeInEvents:[],
+          timeOutEvents:[]
+}
+}
+
+let twoRows = [
+        ["moe", "sizlak", "barkeep", 2],
+        ["bartholomew", "simpson", "scamp", 3]
+      ]
+
+function createEmployeeRecords(twoRows){
+  return  twoRows.map(row => createEmployeeRecord(row))
+  }
+
+function createTimeInEvent(employee ,Date){
+  let [date , hour]= Date.split(' ')
+  
+  employee.timeInEvents.push({
+    type:"TimeIn",
+    date:date,
+    hour:parseInt(hour,10)
+    
+  })
+  return employee
+}
+
+
+function createTimeOutEvent(employee ,Date){
+  let [date , hour]= Date.split(' ')
+  
+  employee.timeOutEvents.push({
+    type:"TimeOut",
+    date:date,
+    hour:parseInt(hour,10)
+    
+  })
+  return employee
+}
+
+
+function hoursWorkedOnDate(employee,reqDate){
+  let timeIn = employee.timeInEvents.find(event=>event.date===reqDate)
+ let timeOut = employee.timeOutEvents.find(event=>event.date===reqDate)
+ 
+ return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(employee,reqDate){
+  return hoursWorkedOnDate(employee,reqDate)*employee.payPerHour
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
